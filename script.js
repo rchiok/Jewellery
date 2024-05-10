7909988f6466e910967291505b821908b6637cc4
rchiok <168842974+rchiok@users.noreply.github.com>
2024-05-10 22:51:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let header = document.querySelector('.header-main');
+let navbar = document.querySelector('.navbar');
+let modal = document.getElementById("modal");
+
+document.querySelector('#menu').onclick = () => {
+    navbar.classList.toggle('active');
+}
